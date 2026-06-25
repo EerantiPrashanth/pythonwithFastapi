0c9bf1509c5334b7363eeca5407f68d790582ddb
EerantiPrashanth <prashanth.e@aifalabs.com>
2026-06-25 12:06:01 +0530

    small changes

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -7,8 +7,6 @@ load_dotenv()
 MONGODB_URI = os.getenv("MONGODB_URI")
 DB_NAME = os.getenv("DB_NAME")
 
-print("MONGO_URI:-",MONGODB_URI)
-print("DB_NAME:-",DB_NAME)
 
 client = AsyncIOMotorClient(MONGODB_URI)
 db = client[DB_NAME]
